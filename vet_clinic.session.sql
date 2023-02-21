be72265e429dc532810046d501ba47fbf06d5597
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-21 22:10:39 +0300

    Remove comments

--- vet_clinic.session.sql
+++
@@ -1,6 +0,0 @@
-
--- INSERT INTO public.animals
--- (name,date_of_birth,escape_attempts,neutered,weight_kg)
--- VALUES('Luch','2020-02-03',0,TRUE,17.3);
-
-SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
