41466767fc97bddb7d0e9ff1b309948e9d73c145
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-21 17:34:34 +0300

    create animals table in vet_clinic db

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+	id integer primary KEY NOT NULL,
+	"name" text NOT null,
+	date_of_birth date NOT NULL,
+	escape_attempts integer NOT NULL,
+	neutered boolean NULL,
+	weight_kg decimal NOT NULL
 );
