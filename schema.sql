47d50e2ce4754bfa8427a8426c3e160cd365298f
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-27 17:21:13 +0300

    Update schema

--- schema.sql
+++ schema.sql
@@ -30,26 +30,33 @@ ADD COLUMN species_id integer;
 ALTER TABLE animals
 ADD COLUMN owner_id integer;
 ALTER TABLE animals
-ADD FOREIGN KEY (species_id) REFERENCES species(id);
+ADD FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE;
 ALTER TABLE animals
-ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id) ON DELETE CASCADE;
 
-CREATE TABLE vets (
-	id serial PRIMARY KEY,
-	name text NOT NULL,
-	age integer NOT NULL,
-	date_of_graduation date NOT NULL
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE specializations (
-	vet_id integer NOT NULL REFERENCES vets(id),
-	species_id integer NOT NULL REFERENCES species(id),
+	vet_id integer NOT NULL REFERENCES vets(id) ON DELETE CASCADE,
+	species_id integer NOT NULL REFERENCES species(id) ON DELETE CASCADE,
 	PRIMARY KEY (vet_id, species_id)
 );
 
-CREATE TABLE visits (
-	vet_id integer NOT NULL REFERENCES vets(id),
-	animal_id integer NOT NULL REFERENCES animals(id),
-	visit_date date NOT NULL,
-	PRIMARY KEY (vet_id, animal_id, visit_date)
+CREATE TABLE visits(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY(id)
 );
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX visits_vet_id_asc ON visits(vet_id ASC);
