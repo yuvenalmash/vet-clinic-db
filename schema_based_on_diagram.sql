8283f1fdcbaae8f4404fac8bdb9035597ccb6271
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-28 12:25:25 +0300

    Add invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS patients (
   id INT GENERATED ALWAYS AS IDENTITY,
   "name" VARCHAR(20),
-  date_of_birth DATE
+  date_of_birth DATE,
   PRIMARY KEY(id)
 );
 
@@ -9,5 +9,14 @@ CREATE TABLE IF NOT EXISTS medical_histories (
   id INT GENERATED ALWAYS AS IDENTITY,
   admited_at TIMESTAMP,
   patient_id INT REFERENCES patients(id),
-  status VARCHAR
-)
+  status VARCHAR,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS invoices (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT REFERENCES medical_histories(id)
+);
