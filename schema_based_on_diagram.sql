4728284592faa49dbb0ae515720004990a4256e5
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-28 12:16:04 +0300

    Add medical histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,10 @@ CREATE TABLE IF NOT EXISTS patients (
   date_of_birth DATE
   PRIMARY KEY(id)
 );
+
+CREATE TABLE IF NOT EXISTS medical_histories (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admited_at TIMESTAMP,
+  patient_id INT REFERENCES patients(id),
+  status VARCHAR
+)
